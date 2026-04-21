04289488dbbdbd6c29eec1a105e4e225bb57b6af
victhorhugoo <163677031+victhorhugoo@users.noreply.github.com>
2026-04-21 15:59:52 -0300

    add run key

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,8 +9,11 @@ public class Player : MonoBehaviour
     public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
+    public float speedRun;
 
     public float ForceJump = 2;
+
+    private float _currentSpeed;
 
     private void Update()
     {
@@ -18,17 +21,29 @@ public class Player : MonoBehaviour
         HandleMoviment();
     }
 
+    // Movimentação do player
     private void HandleMoviment()
     {
+        // Fazer o player correr
+        if (Input.GetKey(KeyCode.Z))
+        {
+            _currentSpeed = speedRun;
+        }
+        else
+        {
+            _currentSpeed = speed;
+        }
+
+        // Fazer o player se mover para os lados
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
         }
 
         // Implementando uma fricção para que o player pare de se mover quando não estiver pressionando as setas
@@ -42,6 +57,7 @@ public class Player : MonoBehaviour
         }
     }
 
+    // Fazer o player pular
     private void HandleJump()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
