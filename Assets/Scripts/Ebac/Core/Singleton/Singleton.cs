30ab86afecd59d8c5e1f69ee3a17702fd27cd624
victhorhugoo <163677031+victhorhugoo@users.noreply.github.com>
2026-04-30 11:14:05 -0300

    add art scene and update ItemManager

--- Assets/Scripts/Ebac/Core/Singleton/Singleton.cs
+++ Assets/Scripts/Ebac/Core/Singleton/Singleton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace Ebac.core.Singleton
 { 
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
